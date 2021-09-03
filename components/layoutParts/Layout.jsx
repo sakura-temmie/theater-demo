8b51364058fa1b2c9762c5a8d5f7cbdf3e8ca765
sakura miyamoto <sakura.m.208@gmail.com>
2021-09-03 18:05:45 +0900

    コンポーネントディレクトリ作成
    レイアウトコンポーネント作成
    簡易的なものですが想像しやすいよう、なんとなく見た目作りました。

---
+++ components/layoutParts/Layout.jsx
@@ -0,0 +1,33 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import LayoutButton from "./LayoutButton";
+
+//childrenはインポート先でラップしてる全てのコンテンツが入ってくる
+//titleはLayoutが呼び出された時にページから特定のタイトルを受け取る
+export default function Layout({ children, title = "HP by Nextjs" }) {
+  return (
+    <div className="flex justify-center item-center flex-col min-h-screen text-gray-600 text-sm font-mono">
+      <Head>
+        {/* プロップスで渡された値を動的に受け取る */}
+        <title>{title}</title>
+      </Head>
+      <header>
+        <nav className="bg-gray-100 w-screen">
+          <div className="flex items-center pl-8 h-24">
+            <div className="flex space-x-4">
+              <Link href="/">
+                <a className="text-gray-500 bg-white hover:bg-white px-4 py-3 rounded">Logo</a>
+              </Link>
+              <LayoutButton path="/" title="演出家を探す" />
+              <LayoutButton path="/" title="メッセージ" />
+              <LayoutButton path="/" title="プロフィール" />
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main className="flex flex-1 justify-center items-center flex-col w-screen">
+        {children}
+      </main>
+    </div>
+  )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/layoutParts/Layout"
+
+export default function Home() {
+  return (
+    <>
+      <Layout title="Top">
+        <p className="text-4xl">ここにコンテンツを配置していく</p>
+      </Layout>
+    </>
+  )
+}
