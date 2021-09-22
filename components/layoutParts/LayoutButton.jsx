4cba79254603bb78d4465a6f68e8c449db48068c
sakura miyamoto <sakura.m.208@gmail.com>
2021-09-22 18:35:44 +0900

    ログアウトボタン作成
    ログイン前後の出しわけ必要

--- components/layoutParts/LayoutButton.jsx
+++ components/layoutParts/LayoutButton.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function LayoutButton({ path, title }) {
   return (
     <Link href={path}>
-      <a className="text-gray-400 hover:bg-yellow-50 p-10 rounded cursor-pointer">
+      <a className="text-gray-400 hover:bg-yellow-50 h-24 p-10 cursor-pointer">
         {title}
       </a>
     </Link>
