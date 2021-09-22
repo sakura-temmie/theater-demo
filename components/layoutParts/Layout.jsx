f41a3490fa11e5cefe3cc57092c1646d31ce418d
sakura miyamoto <sakura.m.208@gmail.com>
2021-09-22 18:46:46 +0900

    layoutコンポーネント修正

--- components/layoutParts/Layout.jsx
+++ components/layoutParts/Layout.jsx
@@ -3,25 +3,17 @@ import Link from "next/link";
 import Image from "next/image";
 import banner from "../../public/icon.png";
 import LayoutButton from "./LayoutButton";
-import Cookie from "universal-cookie";
-import { useRouter } from "next/router";
 
-const cookie = new Cookie();
 //childrenはインポート先でラップしてる全てのコンテンツが入ってくる
 //titleはLayoutが呼び出された時にページから特定のタイトルを受け取る
 export default function Layout({ children, title }) {
-  const router = useRouter();
-  const logout = () => {
-    cookie.remove("access_token");
-    router.push("/");
-  };
   return (
-    <div className="fl ex flex-col text-gray-600 text-sm font-sans min-h-screen">
+    <div className="flex flex-col text-gray-600 text-sm font-sans min-h-screen">
       <Head>
         {/* プロップスで渡された値を動的に受け取る */}
         <title>{title}</title>
       </Head>
-      <header className="sticky top-0 z-10 bg-gray-100 w-screen border-b border-gray-200 items-center text-center justify-center h-24">
+      <header className="sticky top-0 z-10 bg-gray-100 w-screen border-b border-gray-200 items-center text-center justify-center">
         <nav>
           <div className="flex items-center h-24 items-center text-center justify-center">
             {/* <div className="container flex items-center"> */}
@@ -29,16 +21,14 @@ export default function Layout({ children, title }) {
             <Link href="/">
               <p className="text-2xl cursor-pointer pl-2">Spot Light</p>
             </Link>
-            <div className="ml-auto flex ">
+            <div className="ml-auto">
               <LayoutButton path="/" title="演出家を探す" />
               <LayoutButton path="/" title="メッセージ" />
-              <LayoutButton path="/director/profile" title="プロフィール" />
-              <p
-                onClick={logout}
-                className="h-24 text-gray-400 bg-yellow-50 hover:bg-yellow-100 p-10 cursor-pointer"
-              >
-                ログアウト
-              </p>
+              <Link href="/director/profile">
+                <a className="text-gray-400 bg-yellow-50 hover:bg-yellow-100 p-10 rounded cursor-pointer">
+                  プロフィール
+                </a>
+              </Link>
             </div>
           </div>
           {/* </div> */}
