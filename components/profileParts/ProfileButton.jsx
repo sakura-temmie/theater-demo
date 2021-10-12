549b62c18e927af9df8fbebd44c4a8c91eb812c2
nakamura takuma <takuma.nakamura1131@gmail.com>
2021-10-12 15:10:20 +0900

    チャット投げます。

--- components/profileParts/ProfileButton.jsx
+++ components/profileParts/ProfileButton.jsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function ProfileButton({ path, title }) {
+export default function ProfileButton({ path, title, action }) {
   return (
     <Link href={path} passHref>
-      <div className="flex justify-end m-4">
+      <div className="flex justify-end m-4" onClick={action}>
         <a className="bg-yellow-400 hover:bg-yellow-500 text-white text-center font-bold py-2 px-4 rounded w-40 cursor-pointer">
           {title}
         </a>
